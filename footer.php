a1c7e30bf8b8427edf714d69853d28e03fa0db09
dmassimi <31727023+dmassimi@users.noreply.github.com>
2018-12-19 09:13:03 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+
+<div id="footer">
+<p>Copyright &copy; http://www.aziendaagricolalolli.com </p>
+<p>SEDE LEGALE: Via Romagnano 67, 03010 PIGLIO (FR) - P.I. 02800590602</p>
+  <P>Num.Rea.FR-178689</P>
+  <p>Indirizzo e-mail PEC:  <a href="mailto:fratellilolli@legalmail.com">fratellilolli@legalmail.com</a></p>
+<p>Sito ottimizzato per Google Chrome</p>
+</div>
+
+
+</html>
